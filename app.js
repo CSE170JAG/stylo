673d36c28a47601febad7869ec1343d46cbb2a4d
Andrew Nguyen <anguy95@gmail.com>
2016-10-27 18:59:53 -0700

    Initial Commit

---
+++ app.js
@@ -0,0 +1,204 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var handlebars = require('express3-handlebars')
+
+var Forecast = require('forecast');
+
+//Google Maps API
+var fs = require('fs');
+var readline = require('readline');
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+
+var index = require('./routes/index');
+// Example route
+// var user = require('./routes/user');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('Stylo secret key'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Initialize
+var forecast = new Forecast({
+  service: 'darksky',
+  key: '4996cab08400f882874b1f26572f8172',
+  units: 'celcius',
+  cache: true,      // Cache API requests
+  ttl: {            // How long to cache requests. Uses syntax from moment.js: http://momentjs.com/docs/#/durations/creating/
+    minutes: 27,
+    seconds: 45
+  }
+});
+
+function getWeather(){
+  // Retrieve weather information from coordinates (Sydney, Australia)
+  forecast.get([-33.8683, 151.2086], function(err, weather) {
+    if(err) return console.dir(err);
+    return(weather);
+  });
+}
+
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+// Add routes here
+app.get('/', index.view);
+app.post('/', function (req, res) {
+  res.send('Got a POST request');
+});
+
+
+
+// Example route
+// app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+// // If modifying these scopes, delete your previously saved credentials
+// // at ~/.credentials/calendar-nodejs-quickstart.json
+// var SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+// var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+//     process.env.USERPROFILE) + '/.credentials/';
+// var TOKEN_PATH = TOKEN_DIR + 'calendar-nodejs-quickstart.json';
+//
+// // Load client secrets from a local file.
+// fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+//   if (err) {
+//     console.log('Error loading client secret file: ' + err);
+//     return;
+//   }
+//   // Authorize a client with the loaded credentials, then call the
+//   // Google Calendar API.
+//   authorize(JSON.parse(content), listEvents);
+// });
+//
+// /**
+//  * Create an OAuth2 client with the given credentials, and then execute the
+//  * given callback function.
+//  *
+//  * @param {Object} credentials The authorization client credentials.
+//  * @param {function} callback The callback to call with the authorized client.
+//  */
+// function authorize(credentials, callback) {
+//   var clientSecret = credentials.installed.client_secret;
+//   var clientId = credentials.installed.client_id;
+//   var redirectUrl = credentials.installed.redirect_uris[0];
+//   var auth = new googleAuth();
+//   var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+//
+//   // Check if we have previously stored a token.
+//   fs.readFile(TOKEN_PATH, function(err, token) {
+//     if (err) {
+//       getNewToken(oauth2Client, callback);
+//     } else {
+//       oauth2Client.credentials = JSON.parse(token);
+//       callback(oauth2Client);
+//     }
+//   });
+// }
+//
+// /**
+//  * Get and store new token after prompting for user authorization, and then
+//  * execute the given callback with the authorized OAuth2 client.
+//  *
+//  * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+//  * @param {getEventsCallback} callback The callback to call with the authorized
+//  *     client.
+//  */
+// function getNewToken(oauth2Client, callback) {
+//   var authUrl = oauth2Client.generateAuthUrl({
+//     access_type: 'offline',
+//     scope: SCOPES
+//   });
+//   console.log('Authorize this app by visiting this url: ', authUrl);
+//   var rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+//   });
+//   rl.question('Enter the code from that page here: ', function(code) {
+//     rl.close();
+//     oauth2Client.getToken(code, function(err, token) {
+//       if (err) {
+//         console.log('Error while trying to retrieve access token', err);
+//         return;
+//       }
+//       oauth2Client.credentials = token;
+//       storeToken(token);
+//       callback(oauth2Client);
+//     });
+//   });
+// }
+//
+// /**
+//  * Store token to disk be used in later program executions.
+//  *
+//  * @param {Object} token The token to store to disk.
+//  */
+// function storeToken(token) {
+//   try {
+//     fs.mkdirSync(TOKEN_DIR);
+//   } catch (err) {
+//     if (err.code != 'EEXIST') {
+//       throw err;
+//     }
+//   }
+//   fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+//   console.log('Token stored to ' + TOKEN_PATH);
+// }
+//
+// /**
+//  * Lists the next 10 events on the user's primary calendar.
+//  *
+//  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+//  */
+// function listEvents(auth) {
+//   var calendar = google.calendar('v3');
+//   calendar.events.list({
+//     auth: auth,
+//     calendarId: 'primary',
+//     timeMin: (new Date()).toISOString(),
+//     maxResults: 10,
+//     singleEvents: true,
+//     orderBy: 'startTime'
+//   }, function(err, response) {
+//     if (err) {
+//       console.log('The API returned an error: ' + err);
+//       return;
+//     }
+//     var events = response.items;
+//     if (events.length == 0) {
+//       console.log('No upcoming events found.');
+//     } else {
+//       console.log('Upcoming 10 events:');
+//       for (var i = 0; i < events.length; i++) {
+//         var event = events[i];
+//         var start = event.start.dateTime || event.start.date;
+//         console.log('%s - %s', start, event.summary);
+//       }
+//     }
+//   });
+// }
