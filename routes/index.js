11a4f96876b7c0142d7dee5fbd05354a00e326a0
Andrew Nguyen <anguy95@gmail.com>
2016-11-01 18:38:03 -0700

    Add event button, and list events from JSON

--- routes/index.js
+++ routes/index.js
@@ -4,30 +4,63 @@
  */
 
 exports.view = function(req, res){
-  res.render('index',  {"data":[
-    {
-      "itemImg": "http://placehold.it/100x100",
-      "itemDesc": "Red T-Shirt"
-    },
-    {
-      "itemImg": "http://placehold.it/100x100",
-      "itemDesc": "Red T-Shirt"
-    },
-    {
-      "itemImg": "http://placehold.it/100x100",
-      "itemDesc": "Red T-Shirt"
-    },
-    {
-      "itemImg": "http://placehold.it/100x100",
-      "itemDesc": "Red T-Shirt"
-    },
-    {
-      "itemImg": "http://placehold.it/100x100",
-      "itemDesc": "Red T-Shirt"
-    },
-    {
-      "itemImg": "http://placehold.it/100x100",
-      "itemDesc": "Red T-Shirt"
-    }
-  ]});
+
+  var data = {
+    "clothList":[
+      {
+        "itemImg": "http://placehold.it/100x100",
+        "itemDesc": "Red T-Shirt"
+      },
+      {
+        "itemImg": "http://placehold.it/100x100",
+        "itemDesc": "Red T-Shirt"
+      },
+      {
+        "itemImg": "http://placehold.it/100x100",
+        "itemDesc": "Red T-Shirt"
+      },
+      {
+        "itemImg": "http://placehold.it/100x100",
+        "itemDesc": "Red T-Shirt"
+      },
+      {
+        "itemImg": "http://placehold.it/100x100",
+        "itemDesc": "Red T-Shirt"
+      },
+      {
+        "itemImg": "http://placehold.it/100x100",
+        "itemDesc": "Red T-Shirt"
+      }
+    ],
+
+    "eventList": [
+      {
+        'eventDate': '10/25/16',
+        'eventTitle': 'Business Meeting',
+        'eventDesc': 'Meeting with John Doe for stuff.'
+      },
+      {
+        'eventDate': '10/25/16',
+        'eventTitle': 'Business Meeting',
+        'eventDesc': 'Meeting with John Doe for stuff.'
+      },
+      {
+        'eventDate': '10/25/16',
+        'eventTitle': 'Business Meeting',
+        'eventDesc': 'Meeting with John Doe for stuff.'
+      },
+      {
+        'eventDate': '10/25/16',
+        'eventTitle': 'Business Meeting',
+        'eventDesc': 'Meeting with John Doe for stuff.'
+      },
+      {
+        'eventDate': '10/25/16',
+        'eventTitle': 'Business Meeting',
+        'eventDesc': 'Meeting with John Doe for stuff.'
+      }
+    ]
+  }
+
+  res.render('index', data);
 };
